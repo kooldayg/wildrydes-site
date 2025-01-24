8d432dc7d6a6358a20ccd99c44f6e3e806e23031
kooldayg <kooldayg@gmail.com>
2025-01-24 18:24:43 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_Gaj94FFCa', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'v4o3s8ehqa69s2vu80ao33pup', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
